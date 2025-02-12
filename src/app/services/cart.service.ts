b6378e01b6aa24698aec22a41469c182980f71f1
julio-nt <julio.nt753@gmail.com>
2025-02-12 13:45:04 -0300

    update

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,8 +6,30 @@ import { ProductModel } from '../models/product.type';
 })
 export class CartService {
   cart = signal<{ product: ProductModel; quantity: number }[]>([]);
+  // reduce da quantidade total
 
   addToCart(item: ProductModel, quantity: number) {
+    const productExists = this.cart().find(
+      (cartItem) => cartItem.product.id === item.id
+    );
+
+    if (productExists) {
+      const totalQuantity = quantity + productExists.quantity;
+      const newArr: { product: ProductModel; quantity: number }[] =
+        this.cart().map((cartItem) =>
+          cartItem.product.id === item.id
+            ? {
+                product: cartItem.product,
+                quantity: totalQuantity,
+              }
+            : cartItem
+        );
+
+      this.cart.set(newArr);
+      console.log('Carrinho atual: ', this.cart());
+      return;
+    }
+
     this.cart().push({ product: item, quantity });
     console.log('Carrinho atual: ', this.cart());
   }
